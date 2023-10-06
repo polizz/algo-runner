02db469ff6a601c6f4d605d18ff7cfa1878d35be
polizz <polizz@nowhere>
2023-10-06 15:32:55 -0500

    reduce complexity, switch to iterators

--- algorithms/src/backtracking/nqueens.rs
+++ algorithms/src/backtracking/nqueens.rs
@@ -1,3 +1,4 @@
+use std::iter::zip;
 pub struct Solution {}
 
 impl Solution {
@@ -6,112 +7,33 @@ impl Solution {
     let c = col as usize;
     let n = n as usize;
 
-    // check other columns for collisions
-    for col in c..=0 {
+    for col in 0..c {
       if board[r][col] {
-        println!("-horz col check failed q at -> r:{r} c:{col}");
-        return false;
-      }
-    }
-
-    for col in 0..=c {
-      if board[r][col] {
-        println!("+horz col check failed q at -> r:{r} c:{col}");
         return false;
       }
     }
 
     // check upper left diagonals for collisions
-    let mut ri = Some(r);
-    let mut ci = Some(c);
-    loop {
-      // considered unstable by Rust 1.58 (on leetcode)
-      // (ri, ci) = (ri.unwrap().checked_sub(1), ci.unwrap().checked_sub(1));
-      ri = ri.unwrap().checked_sub(1);
-      ci = ci.unwrap().checked_sub(1);
-      if ri.is_some() && ci.is_some() {
-        if board[ri.unwrap()][ci.unwrap()] {
-          println!(
-            "upper left diag failed q at -> r:{} c:{}",
-            ri.unwrap(),
-            ci.unwrap()
-          );
-          return false;
-        }
-      } else {
-        break;
-      }
-    }
-
-    // check upper right diagonals for collisions
-    let mut ri = Some(r);
-    let mut ci = Some(c);
-    loop {
-      (ri, ci) = (ri.unwrap().checked_sub(1), ci.unwrap().checked_add(1));
-      if ri.is_some() && ci.is_some() && ci.unwrap() < n {
-        if board[ri.unwrap()][ci.unwrap()] {
-          println!(
-            "upper right diag failed q at -> r:{} c:{}",
-            ri.unwrap(),
-            ci.unwrap()
-          );
-          return false;
-        }
-      } else {
-        break;
+    let r_iter = (0..r).rev();
+    let c_iter = (0..c).rev();
+    let pair_iter = zip(r_iter, c_iter);
+    for (ri, ci) in pair_iter {
+      if board[ri][ci] {
+        return false;
       }
     }
 
     // check lower left diagonals for collisions
-    let mut ri = Some(r);
-    let mut ci = Some(c);
-    loop {
-      (ri, ci) = (ri.unwrap().checked_add(1), ci.unwrap().checked_sub(1));
-      if ri.is_some() && ci.is_some() && ri.unwrap() < n {
-        if board[ri.unwrap()][ci.unwrap()] {
-          println!(
-            "lower left diag failed q at -> r:{} c:{}",
-            ri.unwrap(),
-            ci.unwrap()
-          );
-          return false;
-        }
-      } else {
-        break;
-      }
-    }
-
-    // check lower right diagonals for collisions
-    let mut ri = Some(r);
-    let mut ci = Some(c);
-    loop {
-      (ri, ci) = (ri.unwrap().checked_add(1), ci.unwrap().checked_add(1));
-      if ri.unwrap() < n && ci.unwrap() < n {
-        if board[ri.unwrap()][ci.unwrap()] {
-          println!(
-            "lower right diag failed q at -> r:{} c:{}",
-            ri.unwrap(),
-            ci.unwrap()
-          );
-          return false;
-        }
-      } else {
-        break;
+    let r_iter = (r + 1)..n;
+    let c_iter = (0..c).rev();
+    let pair_iter = zip(r_iter, c_iter);
+    for (ri, ci) in pair_iter {
+      if board[ri][ci] {
+        return false;
       }
     }
 
     true
-    // for pos in r..=0 {
-    //   if board[pos][c] {
-    //     return false;
-    //   }
-    // }
-    //
-    // for pos in 0..=r {
-    //   if board[pos][c] {
-    //     return false;
-    //   }
-    // }
   }
 
   pub fn place_queen(board: &mut Vec<Vec<bool>>, row: i32, col: i32) {
@@ -129,13 +51,7 @@ impl Solution {
     good_boards: &mut i32,
     d: u32,
   ) -> bool {
-    println!(
-      "d:{d} n:{n} col:{col} good_boards:{good_boards} board:{:#?}",
-      &board
-    );
     if col == n {
-      // good solution, return true
-      println!("good board found");
       *good_boards = *good_boards + 1;
       return true;
     }
@@ -143,9 +59,7 @@ impl Solution {
     // find an empty row starting at 0
     for r in 0..n {
       // in current col, startig at 0 row, try placing queen and check if queen is safe
-      println!("d:{d} main loop checking... col:{} r:{} ", col, r);
       if Solution::is_safe(n, &board, r, col) {
-        println!("d:{d} placing q in row: {r} col: {col}");
         Solution::place_queen(&mut board, r, col);
         Solution::queens_r(n, &mut board, col + 1, good_boards, d + 1);
         Solution::remove_queen(&mut board, r, col);
@@ -161,7 +75,6 @@ impl Solution {
     let u_n = n as usize;
     let mut board = vec![vec![false; u_n]; u_n];
     let mut num_good_boards = 0;
-    // println!("board: {:#?}", &board);
 
     Solution::queens_r(n, &mut board, 0, &mut num_good_boards, 0);
 
